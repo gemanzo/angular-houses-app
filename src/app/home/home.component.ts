285e0727f5e06624617b589c8af5c888e7541b84
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-01-14 17:51:40 +0100

    feat: added housing service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
@@ -16,23 +17,19 @@ import { HousingLocation } from '../housinglocation';
     </section>
     <section class="results">
       <app-housing-location
+        *ngFor="let housingLocation of housingLocationList"
         [housingLocation]="housingLocation"
-      ></app-housing-location>
+      >
+      </app-housing-location>
     </section>
   `,
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+  HousingLocationList: HousingLocation[] = [];
+  HousingService: HousingService = inject(HousingService);
 
-  housingLocation: HousingLocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+  constructor() {
+    this.HousingLocationList = this.HousingService.getAllHousingLocations();
+  }
 }
